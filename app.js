9525fe83453ab7f4515df8ad9b151d101128eff9
Sagardeepsingh21 <sagardeepsingh439@gmail.com>
2026-03-15 11:01:51 +0530

    Added Show in listing

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const ejsMate= require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js")
 const ExpressError = require("./utils/ExpressError.js");
 const {listingSchema} = require("./schema.js");
-
+const Review= require("./models/review.js");
 
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 
@@ -106,6 +106,20 @@ app.get("/listings",wrapAsync(async(req,res)=>{
     })
 );
     
+// Reiveiws
+// POST ROUTE
+    app.post("/listings/:id/reviews", wrapAsync(async(req,res)=>{
+          let listing =await Listing.findById(req.params.id);
+          let newReview = new Review(req.body.review);
+
+          listing.reviews.push(newReview);
+
+          await newReview.save();
+          await listing.save();
+
+          console.log("new review saved");
+          res.redirect(`/listings/${req.params.id}`);
+    }));
 // app.get("/testListing",async(req,res)=>{
 //     let sampleListing=new Listing({
 //         title:"My New Villa",
