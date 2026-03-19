d954b17f549d676ee5a53ebf18b80bac752fd3e1
Sagardeepsingh21 <sagardeepsingh439@gmail.com>
2026-03-19 18:08:13 +0530

    Added the error and deletion flash

--- routes/listing.js
+++ routes/listing.js
@@ -31,6 +31,10 @@ router.get("/",wrapAsync(async(req,res)=>{
     router.get("/:id",wrapAsync(async(req,res)=>{
         let{ id} = req.params;
         const listing = await Listing.findById(id).populate("reviews");
+        if(!listing){
+                    req.flash("error","Listing you requested for does not exist!");
+                    res.redirect("/listings");
+        }
         res.render("listings/show.ejs",{listing});
     }));
 
