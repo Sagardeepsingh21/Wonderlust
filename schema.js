d541f3d89459c210795f05b691c9d930c43873bb
Sagardeepsingh21 <sagardeepsingh439@gmail.com>
2026-03-19 17:27:46 +0530

    Trouble shooting some errors regarding new listing

--- schema.js
+++ schema.js
@@ -1,13 +1,15 @@
 const Joi = require("joi");
 
 const listingSchema = Joi.object({
-    title: Joi.string().required(),
-    description: Joi.string().required(),
-    country: Joi.string().required(),
-    location: Joi.string().required(),
-    price: Joi.number().required()
+    listing: Joi.object({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        image: Joi.string().required(),
+        price: Joi.number().required(),
+        country: Joi.string().required(),
+        location: Joi.string().required()
+    }).required()
 });
-
 const reviewSchema = Joi.object({
     review : Joi.object({
         rating : Joi.number().required().min(1).max(5),
