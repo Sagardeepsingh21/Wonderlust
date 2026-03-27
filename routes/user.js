146c57caf649bbcd7217c4656923876b0f799747
Sagardeepsingh21 <sagardeepsingh439@gmail.com>
2026-03-27 15:05:44 +0530

    Added MVC in user.js

--- routes/user.js
+++ routes/user.js
@@ -5,33 +5,13 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
 
-router.get("/signup",(req,res)=>{
-    res.render("users/signup.ejs");
-});
+const userController = require("../controllers/users.js");
 
-router.post("/signup" ,wrapAsync(async (req,res)=>{
-try{
-    let{username,email,password} = req.body;
-     const newUser = new User({email,username});
-     const registeredUser=await User.register(newUser ,password);
-     console.log(registeredUser);
-     req.login(registeredUser,(err)=>{
-        if(err){
-            return next(err);
-        }
-        req.flash("success","welcome to wonderlust");
-     res.redirect("/listings");
-     });
-     
-} catch(e) {
-    req.flash("error", e.message);
-    res.redirect("/signup");
-}
-}));
+router.get("/signup",userController.renderSignupForm);
 
-router.get("/login",(req,res)=>{
-    res.render("users/login.ejs");
-});
+router.post("/signup" ,wrapAsync(userController.signup));
+
+router.get("/login",userController.renderLoginForm);
 
 router.post("/login",
     saveRedirectUrl,
@@ -39,22 +19,10 @@ router.post("/login",
         failureRedirect : "/login",
         failureFlash: true,
     }),
-    async(req,res)=>{
-        req.flash("success","Welcome to Wonderlust You are Logged In! ");
-        let redirectUrl = res.locals.redirectUrl || "/listings";
-        res.redirect(redirectUrl);
-    }
+   userController.login
 );
 
-router.get("/logout",(req,res,next)=>{
-    req.logout((err)=>{
-        if(err){
-            return next(err);
-        }
-        req.flash("success","you are logged out!");
-        res.redirect("/listings");
-    });
-});
+router.get("/logout",userController.logout);
 
 
 module.exports = router;
